b1ccb056f4946894f1821c5dfe562f441b128601
Double-Domination <squremeta@gmail.com>
2022-02-21 13:09:41 +0700

    feat: sprint game launcher buttons correct implementation

--- src/ts/SprintPage/SprintPage.ts
+++ src/ts/SprintPage/SprintPage.ts
@@ -2,21 +2,57 @@ import { IPage } from '../common/IPage';
 import Menu from '../common/Menu';
 import { PageContext } from '../common/types';
 
-// import domFactory from './domFactory';
-// import './sprint.css';
 import SprintGameView from './sView';
 
 export default class SprintPage extends IPage {
   context: PageContext;
 
+  private selectedLevel = '1';
+
   constructor(ctx: PageContext) {
     super();
     this.context = ctx;
 
-    // working block
-    let sprintComponent = document.querySelector('.start-game');
+    // attach handler for level selection
+    const levelSelectorBlocks = document.querySelectorAll(
+      '#lev-1, #lev-2, #lev-3, #lev-4, #lev-5, #lev-6',
+    );
+    levelSelectorBlocks.forEach((item) => {
+      item.addEventListener('click', (evt: Event) => {
+        this.sprintGameSelectHandler(evt);
+      });
+    });
+
+    // attach handlers for game start
+
+    const startSprintGameButton = document.querySelector('.play-game-btn');
+    startSprintGameButton.addEventListener('click', (evt: Event) =>
+      this.sprintGameStartingHandler(evt),
+    );
+  }
+
+  sprintGameSelectHandler(event: Event) {
+    // console.log(event.currentTarget);
+    const levelSelectors = document.querySelectorAll(
+      '#lev-1, #lev-2, #lev-3, #lev-4, #lev-5, #lev-6',
+    );
+
+    // reset selected btn
+    levelSelectors.forEach((item) => {
+      item.classList.remove('level-chosen');
+    });
+
+    (event.target as HTMLElement).classList.toggle('level-chosen');
+
+    this.selectedLevel = (event.target as HTMLElement).textContent.toString();
+    console.log(this.selectedLevel, 'selected level');
+  }
+
+  async sprintGameStartingHandler(event: Event) {
+    // async call here
+
+    const sprintComponent = document.querySelector('.start-game');
     sprintComponent.replaceChildren(new SprintGameView().domNode);
-    // sprintComponent = new SprintGameView();
   }
 
   render(): Promise<boolean> {
